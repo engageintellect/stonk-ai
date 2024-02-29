25b087d7123e1398149e2a6628c07e9cef966012
engageintellect <jc9361@gmail.com>
2024-02-29 00:42:46 -0800

    init

--- src/routes/ticker/[slug]/+page.server.ts
+++ src/routes/ticker/[slug]/+page.server.ts
@@ -1,7 +1,9 @@
-import type { PageServerLoad } from './$types';
+import type { PageServerLoad } from "./$types";
 
-export const load: PageServerLoad = async ({ params }) => {
-	const res = await fetch(`http://45.56.88.245:8000/api/stock/${params.slug}`);
-	const data = await res.json();
-	return data;
+export const load: PageServerLoad = async ({ params }: any) => {
+  const res = await fetch(
+    `http://engage-dev.com:8000/api/stock/${params.slug}`
+  );
+  const data = await res.json();
+  return data;
 };
