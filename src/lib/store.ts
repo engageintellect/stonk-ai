1753d744bd1a5ae0ec2a9233884fc1944efb63a4
engageintellect <jc9361@gmail.com>
2024-03-12 18:06:59 -0700

    init

--- src/lib/store.ts
+++ src/lib/store.ts
@@ -3,6 +3,6 @@
 import { writable } from 'svelte/store';
 
 // Create a writable store with an initial value
-export const selectedTheme = writable('halloween');
+export const selectedTheme = writable('dim');
 export const searchTicker = writable('');
 export const prefferedTimeframe = writable<number>(180);
