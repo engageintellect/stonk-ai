9abe7c17f9516671cf208b33b0a194314e2e26a6
engageintellect <jc9361@gmail.com>
2024-03-24 22:25:53 -0700

    init

---
+++ src/lib/components/base/YouTubeModal.svelte
@@ -0,0 +1,12 @@
+<!-- The button to open modal -->
+<!-- <label for="my_modal_7" class="btn">open modal</label> -->
+
+<!-- Put this part before </body> tag -->
+<input type="checkbox" id="YouTubeModal" class="modal-toggle" />
+<div class="modal" role="dialog">
+	<div class="modal-box">
+		<h3 class="text-lg font-bold">Hello!</h3>
+		<p class="py-4">This modal works with a hidden checkbox!</p>
+	</div>
+	<label class="modal-backdrop" for="my_modal_7">Close</label>
+</div>
