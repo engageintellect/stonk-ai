0e3139348e0ace8bfc6786b5a257173a11c2d094
engageintellect <jc9361@gmail.com>
2024-04-16 21:00:04 -0700

    init

--- svelte.config.js
+++ svelte.config.js
@@ -1,4 +1,5 @@
-import adapter from '@sveltejs/adapter-auto';
+// import adapter from '@sveltejs/adapter-auto';
+import adapter from '@sveltejs/adapter-vercel'
 import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
 
 /** @type {import('@sveltejs/kit').Config} */
@@ -8,11 +9,19 @@ const config = {
 	preprocess: [vitePreprocess({})],
 
 	kit: {
+
+    adapter: adapter({
+			maxDuration: 60,
+		}),
+
+
 		// adapter-auto only supports some environments, see https://kit.svelte.dev/docs/adapter-auto for a list.
 		// If your environment is not supported or you settled on a specific environment, switch out the adapter.
 		// See https://kit.svelte.dev/docs/adapters for more information about adapters.
-		adapter: adapter()
 	}
 };
 
 export default config;
+
+
+
