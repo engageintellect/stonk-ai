56a4b27db4767f2a4a0027359ee139272aa49530
engageintellect <jc9361@gmail.com>
2024-06-24 21:31:32 -0700

    cleaning up

--- client/src/lib/components/base/Nav.svelte
+++ client/src/lib/components/base/Nav.svelte
@@ -1,77 +1,91 @@
 <script lang="ts">
-	import daisyuiColors from 'daisyui/src/theming/themes';
-	import { selectedTheme } from '$lib/store';
-	import { onMount } from 'svelte';
-	import { customThemeNames } from '$lib/custom-themes';
-	import defaultAvatar from '$lib/assets/default-avatar.jpg';
+  import daisyuiColors from "daisyui/src/theming/themes";
+  import { selectedTheme } from "$lib/store";
+  import { onMount } from "svelte";
+  import { customThemeNames } from "$lib/custom-themes";
+  import defaultAvatar from "$lib/assets/default-avatar.jpg";
 
-	import Icon from '@iconify/svelte';
+  import Icon from "@iconify/svelte";
 
-	let themes = Object.keys(daisyuiColors);
+  let themes = Object.keys(daisyuiColors);
 
-	// push all items from customThemes to themes
-	themes.push(...customThemeNames);
+  // push all items from customThemes to themes
+  themes.push(...customThemeNames);
 
-	// Read the selected theme from local storage or cookies when the component mounts
-	onMount(() => {
-		const storedTheme = localStorage.getItem('selectedTheme');
-		if (storedTheme && themes.includes(storedTheme)) {
-			selectedTheme.set(storedTheme);
-			document.documentElement.setAttribute('data-theme', storedTheme);
-		}
-	});
+  // Read the selected theme from local storage or cookies when the component mounts
+  onMount(() => {
+    const storedTheme = localStorage.getItem("selectedTheme");
+    if (storedTheme && themes.includes(storedTheme)) {
+      selectedTheme.set(storedTheme);
+      document.documentElement.setAttribute("data-theme", storedTheme);
+    }
+  });
 
-	// Function to handle theme selection
-	function handleThemeChange(event: any) {
-		const theme = event.target.value;
-		selectedTheme.set(theme);
-		// Save the selected theme to local storage or cookies
-		localStorage.setItem('selectedTheme', theme);
-	}
+  // Function to handle theme selection
+  function handleThemeChange(event: any) {
+    const theme = event.target.value;
+    selectedTheme.set(theme);
+    // Save the selected theme to local storage or cookies
+    localStorage.setItem("selectedTheme", theme);
+  }
 </script>
 
 <div class="navbar bg-base-100 z-20">
-	<div class="mx-auto w-full max-w-3xl">
-		<div class="flex-1">
-			<a href="/" class="btn btn-primary text-xl">
-				<Icon icon="icon-park-solid:stock-market" class="h-7 w-7" /></a
-			>
-		</div>
+  <div class="mx-auto w-full max-w-3xl">
+    <div class="flex-1">
+      <a href="/" class="btn btn-primary text-xl">
+        <Icon icon="icon-park-solid:stock-market" class="h-7 w-7" /></a
+      >
+    </div>
 
-		<div class="flex items-center">
-			<div class="dropdown dropdown-end">
-				<div tabindex="0" role="button" class="md:tooltip md:tooltip-left" data-tip="select theme">
-					<!-- Add dropdown icon here -->
-					<div class="btn btn-ghost flex items-center gap-2">
-						<!-- <div class="text-lg font-thin">Theme</div> -->
-						<Icon icon="gridicons:themes" class="h-7 w-7" />
-					</div>
-				</div>
-				<ul
-					tabindex="-1"
-					class="dropdown-content bg-base-100 border-primary rounded-box dropdown-end -z-[-1] mt-3 h-96 w-52 overflow-auto border p-2 shadow"
-				>
-					{#each themes.sort() as theme}
-						<li>
-							<input
-								type="radio"
-								name="theme-dropdown"
-								class="theme-controller btn btn-sm btn-block btn-ghost justify-start font-thin"
-								aria-label={theme}
-								value={theme}
-								on:change={handleThemeChange}
-							/>
-						</li>
-					{/each}
-				</ul>
-			</div>
+    <div class="flex items-center">
+      <div class="dropdown dropdown-end">
+        <div
+          tabindex="0"
+          role="button"
+          class="md:tooltip md:tooltip-left"
+          data-tip="select theme"
+        >
+          <!-- Add dropdown icon here -->
+          <div class="btn btn-ghost flex items-center gap-2">
+            <!-- <div class="text-lg font-thin">Theme</div> -->
+            <Icon icon="gridicons:themes" class="h-7 w-7" />
+          </div>
+        </div>
+        <ul
+          tabindex="-1"
+          class="dropdown-content bg-base-100 border-primary rounded-box dropdown-end -z-[-1] mt-3 h-96 w-52 overflow-auto border p-1 shadow"
+        >
+          <li
+            class="sticky top-0 theme-controller text-primary-content btn btn-primary btn-sm btn-block justify-start font-medium mb-2"
+            aria-label={`Current Theme: ${$selectedTheme}`}
+          >
+            <div class="flex items-center gap-2">
+              <Icon icon="mdi-done" class="w-5 h-5" />
+              {$selectedTheme}
+            </div>
+          </li>
+          {#each themes.sort() as theme}
+            <li>
+              <input
+                type="radio"
+                name="theme-dropdown"
+                class="theme-controller btn btn-sm btn-block btn-ghost justify-start font-thin"
+                aria-label={theme}
+                value={theme}
+                on:change={handleThemeChange}
+              />
+            </li>
+          {/each}
+        </ul>
+      </div>
 
-			<div class="flex-none gap-2">
-				<!-- <div class="form-control">
+      <div class="flex-none gap-2">
+        <!-- <div class="form-control">
 			<input type="text" placeholder="Search" class="input input-bordered w-24 md:w-auto" />
 		</div> -->
 
-				<!-- <div class="dropdown dropdown-end">
+        <!-- <div class="dropdown dropdown-end">
 					<div tabindex="0" role="button" class="btn btn-ghost btn-circle avatar">
 						<div class="border-primary w-10 rounded-full border-2">
 							<img alt="Tailwind CSS Navbar component" src={defaultAvatar} />
@@ -91,7 +105,7 @@
 						<li><a href="/" class="text-lg">Logout</a></li>
 					</ul>
 				</div> -->
-			</div>
-		</div>
-	</div>
+      </div>
+    </div>
+  </div>
 </div>
--- client/src/lib/components/base/Roadmap.svelte
+++ client/src/lib/components/base/Roadmap.svelte
@@ -1,67 +1,73 @@
 <script lang="ts">
-	import StockMarket from 'virtual:icons/icon-park-solid/stock-market';
-	import RobotIcon from 'virtual:icons/mdi/robot';
-	import NewsIcon from 'virtual:icons/fluent/news-24-filled';
-	import YouTubeIcon from 'virtual:icons/tabler/brand-youtube-filled';
-	import BrainIcon from 'virtual:icons/lucide/brain';
-	import { fade } from 'svelte/transition';
+  import Icon from "@iconify/svelte";
+  import { fade } from "svelte/transition";
 </script>
 
 <div class="mx-auto w-full max-w-lg md:max-w-full">
-	<div class="text-center text-2xl font-semibold sm:text-3xl">
-		Everything you need, in one place.
-	</div>
-	<ul
-		transition:fade={{ delay: 0, duration: 200 }}
-		class="timeline timeline-snap-icon max-md:timeline-compact timeline-vertical py-5"
-	>
-		<li>
-			<div class="timeline-middle px-5">
-				<StockMarket class="text-primary h-10 w-10" />
-			</div>
-			<div class="timeline-start md:text-end">
-				<!-- <time class="font-mono italic">Detailed stock market insights</time> -->
-				<div class="text-primary text-xl font-black">Detailed stock market insights</div>
-				Stonk AI provides detailed stock market insights for any company.
-			</div>
-			<hr />
-		</li>
-		<li>
-			<hr />
-			<div class="timeline-middle text-primary px-5">
-				<BrainIcon class="h-10 w-10" />
-			</div>
-			<div class="timeline-end">
-				<!-- <time class="font-mono italic">AI generated predictions and input</time> -->
-				<div class="text-primary text-xl font-black">AI generated predictions and input</div>
-				We use AI to generate predictions and input for the stock market. We provide the latest stock
-				information for any company
-			</div>
-			<hr />
-		</li>
-		<li>
-			<hr />
-			<div class="timeline-middle text-primary px-5">
-				<NewsIcon class="h-10 w-10" />
-			</div>
-			<div class="timeline-start md:text-end">
-				<!-- <time class="font-mono italic">The latest stock news</time> -->
-				<div class="text-primary text-xl font-black">The latest stock news</div>
-				Stonk AI provides the latest stock news for any company. for any company
-			</div>
-			<hr />
-		</li>
-		<li>
-			<hr />
-			<div class="timeline-middle text-primary px-5">
-				<YouTubeIcon class="h-10 w-10" />
-			</div>
-			<div class="timeline-end">
-				<!-- <time class="font-mono italic">Recent videos</time> -->
-				<div class="text-primary text-xl font-black">Recent videos</div>
-				Stonk AI provides recent videos for any company.
-			</div>
-			<hr />
-		</li>
-	</ul>
+  <div class="text-center text-2xl font-semibold sm:text-3xl">
+    Everything you need, in one place.
+  </div>
+  <ul
+    transition:fade={{ delay: 0, duration: 200 }}
+    class="timeline timeline-snap-icon max-md:timeline-compact timeline-vertical py-5"
+  >
+    <li>
+      <div class="timeline-middle px-5">
+        <Icon
+          icon="icon-park-solid:stock-market"
+          class="text-primary h-10 w-10"
+        />
+      </div>
+      <div class="timeline-start md:text-end">
+        <!-- <time class="font-mono italic">Detailed stock market insights</time> -->
+        <div class="text-primary text-xl font-black">
+          Detailed stock market insights
+        </div>
+        Stonk AI provides detailed stock market insights for any company.
+      </div>
+      <hr />
+    </li>
+    <li>
+      <hr />
+      <div class="timeline-middle text-primary px-5">
+        <Icon icon="lucide:brain" class="text-primary h-10 w-10" />
+      </div>
+      <div class="timeline-end">
+        <!-- <time class="font-mono italic">AI generated predictions and input</time> -->
+        <div class="text-primary text-xl font-black">
+          AI generated predictions and input
+        </div>
+        We use AI to generate predictions and input for the stock market. We provide
+        the latest stock information for any company
+      </div>
+      <hr />
+    </li>
+    <li>
+      <hr />
+      <div class="timeline-middle text-primary px-5">
+        <Icon icon="fluent:news-24-filled" class="text-primary h-10 w-10" />
+      </div>
+      <div class="timeline-start md:text-end">
+        <!-- <time class="font-mono italic">The latest stock news</time> -->
+        <div class="text-primary text-xl font-black">The latest stock news</div>
+        Stonk AI provides the latest stock news for any company. for any company
+      </div>
+      <hr />
+    </li>
+    <li>
+      <hr />
+      <div class="timeline-middle text-primary px-5">
+        <Icon
+          icon="tabler:brand-youtube-filled"
+          class="text-primary h-10 w-10"
+        />
+      </div>
+      <div class="timeline-end">
+        <!-- <time class="font-mono italic">Recent videos</time> -->
+        <div class="text-primary text-xl font-black">Recent videos</div>
+        Stonk AI provides recent videos for any company.
+      </div>
+      <hr />
+    </li>
+  </ul>
 </div>
